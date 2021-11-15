24aae5bf78e581c477987e6c27bc7291ba06dc32
Skylar Grant <official.xian@gmail.com>
2021-11-15 13:26:34 -0500

    .

--- Lab13_demo.c
+++ Lab13_demo.c
@@ -10,28 +10,27 @@
 */
 
 // Variable for counting
-int i,j;
-int col[8] = {1,2,4,8,16,32,64,128};
-int row[8] = {128,64,32,16,8,4,2,1};
-
-// Set Ports B and C for output and clear them
-TRISB = 0;
-TRISC = 0;
-PORTB = 0;
-PORTC = 0;
+int i, j;
+int col[8] = {1, 2, 4, 8, 16, 32, 64, 128};
+int row[8] = {128, 64, 32, 16, 8, 4, 2, 1};
 
 void main()
 {
-	while (1)
-	{
-		for (i = 0; i < 8; i++)
-		{
-			PORTB = ~col[i];
-			for (j = 0; j < 8; j++)
-			{
-				PORTC = ~row[j];
-				delay_ms(500);
-			}
-		}
-	}
+    // Set Ports B and C for output and clear them
+    TRISB = 0;
+    TRISC = 0;
+    PORTB = 0;
+    PORTC = 0;
+    while (1)
+    {
+        for (i = 0; i < 8; i++)
+        {
+            PORTB = ~col[i];
+            for (j = 0; j < 8; j++)
+            {
+                PORTC = ~row[j];
+                delay_ms(500);
+            }
+        }
+    }
 }
